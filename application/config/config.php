7bada939336f18416f1c586f61f6107dd7e1224f
Dhatraditya J <aditya@digitalchakra.in>
2012-10-16 16:15:51 +0530

    Session variables set for login. Do NOT modify

--- application/config/config.php
+++ application/config/config.php
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = '5Hh85CS292Pjj0Rkw3nz4jtXHd4Y3229';
 
 /*
 |--------------------------------------------------------------------------
