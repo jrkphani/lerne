de28d172e58b94092471dc12782284ad84b5c398
Niranjan <niranjan.prithviraj@gmail.com>
2012-10-16 14:20:07 +0530

    listing of anwers done

---
+++ application/models/answer_model.php
@@ -0,0 +1,12 @@
+<?php
+class Answer_model extends CI_Model {
+    function __construct(){
+        parent::__construct();
+    }
+	function search($params){
+		$this->db->like('question_id',$params['qid']);
+		$query = $this->db->get('answers');
+		return $query->result();
+	}
+}
+?>
