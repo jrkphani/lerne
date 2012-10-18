ae8eb99563e25d33b9e9d10b71212bd7b8072e40
Dhatraditya J <aditya@digitalchakra.in>
2012-10-18 15:05:15 +0530

    Preserving session on login page

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -6,10 +6,14 @@ class Login extends CI_Controller {
   {
     parent::__construct();
     $this->load->helper('form');
+    $this->load->helper('url');
   }
 
   function index()
   {
+  	if($this->session->userdata('logged_in')){
+  		redirect('home', 'refresh');
+  	}
     $this->load->view('login_view');
   }
 
