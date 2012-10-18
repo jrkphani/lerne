affe28a4ffbdb7467b1216143ea147d078a53977
Dhatraditya J <aditya@digitalchakra.in>
2012-10-18 15:11:39 +0530

    made home as the default controller

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -6,14 +6,10 @@ class Login extends CI_Controller {
   {
     parent::__construct();
     $this->load->helper('form');
-    $this->load->helper('url');
   }
 
   function index()
   {
-  	if($this->session->userdata('logged_in')){
-  		redirect('home', 'refresh');
-  	}
     $this->load->view('login_view');
   }
 
