cb61c511f300b827083443912c113eb7d811e284
Dhatraditya J <aditya@digitalchakra.in>
2012-10-18 14:58:28 +0530

    minor changes to login_view

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -1,17 +1,15 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
   <head>
-    <title>Simple Login with CodeIgniter</title>
+    <title>Welcome to Lerne!</title>
   </head>
   <body>
-    <h1>Simple Login with CodeIgniter</h1>
+    
     <?php echo validation_errors(); ?>
     <?php echo form_open('verifylogin'); ?>
-      <label for="email">Email:</label>
-      <input type="text" size="20" id="email" name="email"/>
+      <input type="text" size="20" id="email" name="email" placeholder="Email"/>
       <br/>
-      <label for="password">Password:</label>
-      <input type="password" size="20" id="passowrd" name="password"/>
+      <input type="password" size="20" id="passowrd" name="password" placeholder="Password"/>
       <br/>
       <input type="submit" value="Login"/>
     </form>
