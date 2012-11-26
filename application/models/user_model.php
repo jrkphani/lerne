a55340c5c386c186c1b9382c10afd62ff9b4c43e
Dhatraditya J <aditya@digitalchakra.in>
2012-11-26 11:23:31 +0530

    Taken care of SQL injection upon login

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -3,18 +3,20 @@ Class User_model extends CI_Model
 {
 	function login($email, $password)
 	{
-		$this -> db -> select('*');
-		$this -> db -> from('users');
-		$this -> db -> where('email = ' . "'" . $email . "'"); 
-		$this -> db -> where('password = ' . "'" . MD5($password) . "'"); 
-		$this -> db -> where("confirmcode = '1' ");
-		$this -> db -> limit(1);
-
-		$query = $this -> db -> get();
-
-		if($query -> num_rows() == 1)
+		$sql_login = "select * from users where email = ? and password = ? and confirmcode = ? limit 1";
+		$query_login = $this->db->query($sql_login, array($email, md5($password), 1));
+		/*
+		$this->db->select('*');
+		$this->db->from('users');
+		$this->db->where('email', $email);
+		$this->db->where('password', md5($password)); 
+		$this->db->where('confirmcode', 1);
+		$this->db->limit(1);
+		$query = $this->db->get();
+		*/
+		if($query_login->num_rows() == 1)
 		{
-			return $query->result();
+			return $query_login->result();
 		}
 		else
 		{
@@ -45,6 +47,7 @@ Class User_model extends CI_Model
   $this->db->set('user_created','NOW()',false);
   $this->db->insert('users',$data);
  }
+ 
 }
 
 ?>
